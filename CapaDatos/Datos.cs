061956879efbfe8497e21be7981d000789b186d7
mikeangeldelgado <miakangeldelgado@gmail.com>
2025-03-04 17:47:57 -0600

    primer commit

---
+++ CapaDatos/Datos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class Datos
+    {
+        public double a = Convert.ToDouble(ConfigurationSettings.AppSettings["Valor_A"].ToString());
+        public double b = Convert.ToDouble(ConfigurationSettings.AppSettings["Valor_B"].ToString());
+        public double c = Convert.ToDouble(ConfigurationSettings.AppSettings["Valor_C"].ToString());
+    }
+}
