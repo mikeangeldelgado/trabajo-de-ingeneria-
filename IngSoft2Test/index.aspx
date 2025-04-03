bec04b9ef0fa13b8441c49033e9047b901784423
mikeangeldelgado <miakangeldelgado@gmail.com>
2025-04-02 20:46:53 -0600

    Subiendo nueva versión del proyecto

---
+++ IngSoft2Test/index.aspx
@@ -0,0 +1,70 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="IngSoft2Test.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Calcular Ecuación - Test</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td>
+                        Ecuación:
+                    </td>
+                    <td>
+                        y = (a)x^2 + b(x) + c
+                    </td>
+                </tr>
+                <tr>
+                    <td align="right">
+                        x =
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txt_X" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2" align="center">
+                        <asp:Button ID="btCalcular" runat="server" Text="Calcular"
+                            OnClick="btCalcular_Click"
+                            OnClientClick="return validarCampo();" />
+                    </td>
+                </tr>
+                <tr>
+                    <td align="right">
+                        y = 
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtResultado" runat="server" ReadOnly="True"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <asp:Label ID="lblMsg" runat="server" Text=""></asp:Label>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+    <script type="text/javascript">
+        function validarCampo() {
+            var campoX = document.getElementById("txt_X").value;
+            var isNumero = false;
+
+            if (isNaN(campoX)) {
+                isNumero = false;
+                alert("Por favor solo capture numeros");
+            }
+            else {
+                isNumero = true;
+            }
+
+            return isNumero;
+        }
+    </script>
+</body>
+</html>
---
+++ IngSoft2Test/index.aspx.designer.cs
@@ -0,0 +1,62 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace IngSoft2Test
+{
+
+
+    public partial class index
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control txt_X.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_X;
+
+        /// <summary>
+        /// Control btCalcular.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btCalcular;
+
+        /// <summary>
+        /// Control txtResultado.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtResultado;
+
+        /// <summary>
+        /// Control lblMsg.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblMsg;
+    }
+}
